2a342ab7a3279cb2c2ae6dfc58b62fa208fbd044
Ricky Garcia <rg3646@outlook.com>
2022-07-06 23:49:23 -0600

    adds firebase user signup

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,7 @@
-import { useState, form } from 'react';
+import { useState } from 'react';
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+
+import FormInput from '../form-input/form-input.component';
 
 const defaultFormFields = {
   displayName: '',
@@ -12,7 +15,29 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
-  console.log(formFields);
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if(password !== confirmPassword) {
+      alert("password do not match");
+      return;
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(email, password);
+      await createUserDocumentFromAuth(user, {displayName});
+      resetFormFields();
+    } catch(error) {
+      console.log(error);
+      if(error.code === 'auth/email-already-in-use') {
+        alert('cannot use email. Already in use.')
+      }
+    }
+  }
 
   const handleChange = (event) => {
     const {name, value} = event.target;
@@ -23,16 +48,11 @@ const SignUpForm = () => {
     <div>
       <h1>Sign up with your email and password</h1>
       
-      <form onSubmit={() => {}}>
-
-        <label>Display Name</label>
-        <input type="text" required onChange={handleChange} name="displayName" value={displayName}/>
-        <label>Email</label>
-        <input type="email" required onChange={handleChange} name="email" value={email}/>
-        <label>Password</label>
-        <input type="password" required onChange={handleChange} name="password" value={password}/>
-        <label>Confirm Password</label>
-        <input type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
+      <form onSubmit={handleSubmit}>
+        <FormInput label="Display Name" type="text" required onChange={handleChange} name="displayName" value={displayName}/>
+        <FormInput label="Email" type="email" required onChange={handleChange} name="email" value={email}/>
+        <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password}/>
+        <FormInput label="Confirm Password" type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
         <button type="submit">Sign Up</button>
       </form>
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,7 +4,8 @@ import {
   getAuth,
   signInWithRedirect,
   signInWithPopup,
-  GoogleAuthProvider
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword
 } from 'firebase/auth';
 
 import {
@@ -41,21 +42,17 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
   const userDocRef = doc(db, 'users', userAuth.uid);
 
-  console.log(userDocRef);
-
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot);
-  console.log(userSnapshot.exists());
 
   if(!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
     try {
-      await setDoc(userDocRef, {displayName, email, createdAt});
+      await setDoc(userDocRef, {displayName, email, createdAt, ...additionalInformation});
     } catch (error) {
       console.log('error creating the user', error.message);
     }
@@ -63,3 +60,9 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 
   return userDocRef;
 }
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
