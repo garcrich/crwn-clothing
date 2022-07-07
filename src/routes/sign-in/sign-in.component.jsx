9aa58b01654e5bb6c485bb3ef1f69a1e0ebf6bec
Ricky Garcia <rg3646@outlook.com>
2022-07-06 22:11:28 -0600

    intial firebase and sign up setup

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,31 @@
-import { signInWithGooglePopPopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import {
+  auth,
+  signInWithGoogleRedirect,
+  signInWithGooglePopup, 
+  createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopPopup();
+    const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-    console.log(userDocRef);
   }
+
 
   return (
     <>
       <h1>Sign In Page</h1>
+      
       <button onClick={logGoogleUser}>
         Sign in with Google Popup
       </button>
+
+      <SignUpForm />
     </>
   )
 }
