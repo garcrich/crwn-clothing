a10913d713c000a10a0a93beea6dae221028016b
Ricky Garcia <rg3646@outlook.com>
2022-07-07 23:55:30 -0600

    adds styling and small refactor

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,28 +1,14 @@
-import {
-  signInWithGooglePopup, 
-  createUserDocumentFromAuth 
-} from '../../utils/firebase/firebase.utils';
+import './authentication.styles.scss';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
-import SignUpForm from '../../components/sign-in-form/sign-in-form.component';
-
-const SignIn = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
-  }
-
-
+const Authentication = () => {
   return (
-    <>
-      <h1>Sign In Page</h1>
-      
-      <button onClick={logGoogleUser}>
-        Sign in with Google Popup
-      </button>
-
+    <div className='authentication-container'>
+      <SignInForm />
       <SignUpForm />
-    </>
+    </div>
   )
 }
 
-export default SignIn
+export default Authentication;
