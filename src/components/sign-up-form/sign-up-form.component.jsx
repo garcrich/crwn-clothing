796db3e6c1a5ad46fa909446a78cbfcffbdd3fa4
Ricky Garcia <rg3646@outlook.com>
2022-07-08 23:22:42 -0600

    sets up context for current user

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
+import { UserContext } from '../../contexts/user.context';
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 import './sign-up-form.styles.scss';
@@ -17,6 +18,8 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
+  const { setCurrentUser } = useContext(UserContext);
+
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   }
@@ -31,6 +34,9 @@ const SignUpForm = () => {
 
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password);
+      
+      setCurrentUser(user);
+      
       await createUserDocumentFromAuth(user, {displayName});
       resetFormFields();
     } catch(error) {
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 // as the actual value you want to access
 export const UserContext = createContext({
   currentUser: null,
-  setcurrentUser: () => null
+  setCurrentUser: () => null
 })
 
 export const UserProvider = ({ children }) => {
