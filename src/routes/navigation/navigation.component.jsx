404663b98ee8217f4e30c426f2e6d4743f5a52d1
Ricky Garcia <rg3646@outlook.com>
2022-07-10 19:25:57 -0600

    creates shopping icon for dropdown menu

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,7 +4,7 @@ import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import { UserContext } from '../../contexts/user.context'
-
+import CartIcon from '../../components/cart-icon/cart-icon.component'
 import './navigation.styles.scss';
 
 const Navigation = () => {
@@ -29,6 +29,7 @@ const Navigation = () => {
               </Link>
             )
           }
+          <CartIcon />
         </div>
       </div>
       <Outlet />
