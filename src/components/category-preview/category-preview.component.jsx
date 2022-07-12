833ba73d5bba141588938105bd1fe62cbb9daf80
Ricky Garcia <rg3646@outlook.com>
2022-07-12 00:19:58 -0600

    sets up category pages

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,12 +1,14 @@
 import ProductCard from '../product-card/product-card.component'
 import './category-preview.styles.scss'
-
+import { Link } from 'react-router-dom'
 const CategoryPreview = ({title, products}) => {
   return (
     <div className='category-preview-container'>
-      <h2>
-        <span className='title'>{title.toUpperCase()}</span>
-      </h2>
+      <Link to={title}>
+        <h2>
+          <span className='title'>{title.toUpperCase()}</span>
+        </h2>
+      </Link>
       <div className='preview'>
         {
           products
