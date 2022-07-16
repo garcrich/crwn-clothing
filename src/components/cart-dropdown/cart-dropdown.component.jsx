46985cbe2110dc53f31e5826d3ad3dd811455fb5
Ricky Garcia <rg3646@outlook.com>
2022-07-15 22:37:13 -0600

    styled components conversion

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,7 +6,7 @@ import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss';
+import {CartDropdownContainer, EmptyMessage, CartItems} from './cart-dropdown.styles.jsx';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
@@ -17,16 +17,16 @@ const CartDropdown = () => {
   }
 
   return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'>
+    <CartDropdownContainer>
+      <CartItems>
         {cartItems.length ?
           cartItems.map((item) => <CartItem key={item.id} cartItem={item}/>) :
-          <span>Your cart is empty</span>
+          <EmptyMessage>Your cart is empty</EmptyMessage>
         }
         
-      </div>
+      </CartItems>
       <Button onClick={goToCheckoutHandler}>Check Out</Button>
-    </div>
+    </CartDropdownContainer>
   )
 }
 
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,16 @@
-
-import { Link } from 'react-router-dom'
-
-import './directory-item.styles.scss';
+import { BackgroundImage, Body,DirectoryItemContainer } from './directory-item.styles.jsx';
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
 
   return (
-    <Link to={`shop/${title}`}className="directory-item-container">
-      <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
-      {/* <img /> */}
-      <div className="body">
+    <DirectoryItemContainer to={`shop/${title}`}>
+      <BackgroundImage imgeUrl={imageUrl} />
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </Link>
+      </Body>
+    </DirectoryItemContainer>
   )
 }
 
