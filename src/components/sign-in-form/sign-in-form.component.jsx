199dfb5e6c0849ade239299338263a2d2c771dd7
Ricky Garcia <rg3646@outlook.com>
2022-07-16 22:30:40 -0600

    fixing eslint errors

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,8 +1,7 @@
-import { useContext, useState } from 'react';
+import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
-import { UserContext } from '../../contexts/user.context'
 import {
   signInWithGooglePopup,
   signInAuthUserWithEmailAndPassword,
