0fa76db2424da0069d0ceb721d2a84266ecfd9f4
Ricky Garcia <rg3646@outlook.com>
2022-07-18 23:57:41 -0600

    updates categories

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,17 +1,20 @@
-import { useContext, useState, useEffect } from 'react'
+
+
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import ProductCard from '../../components/product-card/product-card.component'
-
-import { CategoriesContext } from '../../contexts/categories.context'
+import { selectCategoriesMap } from '../../store/categories/categories.selector'
 
 import './category.styles.scss'
 
 const Category = () => {
   const { category } = useParams()
-  const { categoriesMap } = useContext(CategoriesContext)
+  const categoriesMap = useSelector(selectCategoriesMap)
   const [products, setProducts] = useState(categoriesMap[category]);
-
+  console.log('render/re-rendering category component')
   useEffect(() => {
+    console.log('effect fired calling setProducts');
     setProducts(categoriesMap[category])
   }, [category, categoriesMap])
 
