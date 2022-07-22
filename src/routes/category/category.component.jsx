716181f2800443d000084b1a42f6cad72f658282
Ricky Garcia <rg3646@outlook.com>
2022-07-21 19:50:05 -0600

    adds is loading thunks

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -4,12 +4,14 @@ import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import ProductCard from '../../components/product-card/product-card.component'
-import { selectCategoriesMap } from '../../store/categories/category.selector'
+import Spinner from '../../components/spinner/spinner'
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector'
 
 import './category.styles.scss'
 
 const Category = () => {
   const { category } = useParams()
+  const isLoading = useSelector(selectCategoriesIsLoading)
   const categoriesMap = useSelector(selectCategoriesMap)
   const [products, setProducts] = useState(categoriesMap[category]);
   useEffect(() => {
@@ -19,11 +21,17 @@ const Category = () => {
   return (
     <>
       <h2 className='category-title'>{category.toLocaleUpperCase()}</h2>
-      <div className='category-container'>
-        {products &&
-          products.map((product) => <ProductCard key={product.id} product={product}/>)
-        }
-      </div>
+      {
+        isLoading ? (
+        <Spinner /> 
+        ) : (
+          <div className='category-container'>
+            {products &&
+              products.map((product) => <ProductCard key={product.id} product={product}/>)
+            }
+          </div>
+        )
+      }
     </>
   )
 }
