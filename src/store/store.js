94c7b6305ca91b5003313123aa07504f4a122f65
Ricky Garcia <rg3646@outlook.com>
2022-08-02 23:00:22 -0600

    converting to redux saga

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,13 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-
+import createSagaMiddleware from '@redux-saga/core';
+import { rootSaga } from './root-saga';
 import { rootReducer } from './root-reducer';
-
+const sagaMiddleware = createSagaMiddleware();
 const middleWares = [
   process.env.NODE_ENV === 'development' && logger,
-  thunk,
+  sagaMiddleware,
 ].filter(Boolean);
 
 const composeEnhancer =
@@ -23,6 +23,8 @@ const persistConfig = {
   whitelist: ['cart'],
 };
 
+
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
@@ -32,5 +34,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
